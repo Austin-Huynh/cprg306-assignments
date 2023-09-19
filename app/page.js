b7f1024640ceff19680c491b657bebee6b2c8fef
Austin Huynh <Austin.h54321@gmail.com>
2023-09-19 08:50:48 -0600

    week 3 added

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ export default function Home() {
       <StudentInfo />
       <p>
         <Link href="/week2">Week 2</Link>
+        <br />
+        <Link href="/week3">Week 3</Link>
       </p>
     </main>
   )
---
+++ app/week3/item.js
@@ -0,0 +1,11 @@
+function Item({ name, quantity, category }) {
+    return (
+      <li className="border border-black p-4 mb-4">
+        <strong className="font-bold">{name}</strong>
+        <p>Quantity: {quantity}</p>
+        <p>Category: {category}</p>
+      </li>
+    );
+  }
+  
+  export default Item;
